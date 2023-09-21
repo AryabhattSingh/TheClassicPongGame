02d7d1d2d6e1cac55f9bb927a44f8e8c8ffbed10
Aryabhatt <bhattsahab314@gmail.com>
2023-09-21 23:23:52 +0530

    Created the initial black screen of certain height and width in main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Create a screen
+from turtle import Screen
+
+screen = Screen()
+screen.title("The Classic Pong Game")
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+
+screen.exitonclick()
