aca099230b1c11e7c9d4efcd033f21871e53b6c1
Aryabhatt <bhattsahab314@gmail.com>
2023-09-23 13:03:25 +0530

    Added a Scoreboard class in scoreboard.py. Used it in main.py to display a score board

---
+++ scoreboard.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("white")
+        self.left_score = 0
+        self.right_score = 0
+        self.update_score()
+        self.hideturtle()
+
+    def update_score(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.left_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.right_score, align="center", font=("Courier", 80, "normal"))
